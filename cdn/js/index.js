c087e08b103d628a76156ff78e2336ba38a8f075
xiejunling <xiejunling@bytedance.com>
2020-01-20 14:26:11 +0800

    double kill

--- cdn/js/index.js
+++ cdn/js/index.js
@@ -1,9 +1,7 @@
 /**
- * PC端页面，会自动注入jQuery.js，请勿自行引用
- * 移动端页面，会自动注入zepto.js，请勿自行引用
- * 在移动端请使用轻量级的zepto.js代替jQuery.js
+ * 页面会自动注入jQuery.js，直接使用即可，请勿自行引入
  */
-$.ready(function() {
+$(function() {
   $('#J_btn').click(function() {
     // 上报业务埋点：领奖事件
     var query = GUtils.parseQuery(); // Gutils是内置的工具函数集，文档：https://bytedance.feishu.cn/docs/doccntMkQZ5l5dtX8xn0LOh1lse
