08793db5130ed872a39eeb1d2f9f6931a5670a3e
xiejunling <xiejunling@bytedance.com>
2020-01-21 14:47:13 +0800

    update

--- cdn/js/index.js
+++ cdn/js/index.js
@@ -4,16 +4,11 @@
 $(function() {
   $('#J_btn').click(function() {
     // 上报业务埋点：领奖事件
-    var query = GUtils.parseQuery(); // Gutils是内置的工具函数集，文档：https://bytedance.feishu.cn/docs/doccntMkQZ5l5dtX8xn0LOh1lse
-    GUtils.collectEvent('event_get_award', {
+    var query = GameTool.parseQuery(); // GameTool是内置的工具函数集，文档：https://bytedance.feishu.cn/docs/doccntMkQZ5l5dtX8xn0LOh1lse
+    GameTool.collectEvent('event_get_award', {
       from_source: query.from_source, // 从URL参数中获取投放渠道，并且上报
     });
 
     alert('恭喜您已领取到大奖！');
-  });
-
-  // 调用jsBridge获取appInfo，仅在游戏内或头条APP内有效
-  GUtils.JsBridge.call('appInfo', {}, function(appInfo) {
-    console.log(appInfo);
   });
 });
